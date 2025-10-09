3e9a9617edd2dabd5159d307e65265bb58876c77
rolv <rolv.apneseth@gmail.com>
2025-10-09 10:10:03 +0100

    refactor: additional checks and error messages for box art images
    Additional error messages when validating the provided box art images -
    including a message if `rofi` does not support the given format

--- src/lib.rs
+++ src/lib.rs
@@ -202,8 +202,30 @@ impl<'rofi> rofi_mode::Mode<'rofi> for Mode<'rofi> {
     fn entry_icon(&mut self, line: usize, height: u32) -> Option<rofi_mode::cairo::Surface> {
         let entry = &self.entries[line];
 
+        // Validate box art path
+        let path_str = {
+            let Some(path) = entry.path_box_art.as_ref() else {
+                error!("path to box art for '{}' does not exist", entry.title);
+                return None;
+            };
+            let Some(path_str) = path.to_str() else {
+                error!("path to box art for '{}' is not valid unicode", entry.title);
+                return None;
+            };
+            path_str
+        };
+
+        // Validate box art image format support
+        if !self.api.supports_image(path_str) {
+            error!(
+                "rofi does not support displaying the image format of {}",
+                path_str
+            );
+            return None;
+        };
+
         self.api
-            .query_icon(entry.path_box_art.as_ref()?.to_str()?, height)
+            .query_icon(path_str, height)
             .wait(&mut self.api)
             .ok()
     }
